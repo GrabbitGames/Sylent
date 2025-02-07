83337896776f4bb2e11edabc0c080b83cb4e5d2b
GrabbitGames <154015851+GrabbitGames@users.noreply.github.com>
2025-02-07 16:24:45 -0500

    Update uv.config.js

--- prox/uv/uv.config.js
+++ prox/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/prox/vexxed-browser/',
-    bare: 'https://vercel-bare-server-tau.vercel.app/',
+    bare: 'https://vercel-bare-server-tau.vercel.app/index.js',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/prox/uv/uv.handler.js',
