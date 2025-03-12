6d135fec5728c339d25d4985987d0d69f8d10774
GrabbitGames <154015851+GrabbitGames@users.noreply.github.com>
2025-03-12 12:16:19 -0400

    Update uv.config.js

--- prox/uv/uv.config.js
+++ prox/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/prox/vexxed-browser/',
-    bare: 'https://tomp.app',
+    bare: 'https://v2202412246404304352.megasrv.de/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/prox/uv/uv.handler.js',
